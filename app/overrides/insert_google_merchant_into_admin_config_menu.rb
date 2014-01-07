97dc917375fdbf8ae8eefb0b0ce8ed259ad9dcbe
Enrique Alvarez <enrique@codecantor.com>
2014-01-07 11:59:00 +0100

    Disable google product type, while decide other way of doing it!

--- app/overrides/insert_google_merchant_into_admin_config_menu.rb
+++ app/overrides/insert_google_merchant_into_admin_config_menu.rb
@@ -1,11 +1,11 @@
 Deface::Override.new(
-  :virtual_path => "spree/admin/configurations/index",
+  :virtual_path => "spree/admin/configurations/indexa",
   :name => "insert_google_merchant_into_admin_config_menu",
   :insert_after => "tbody[data-hook='admin_configurations_menu']",
   :text => %(<%= configurations_menu_item(t("google_merchant"), admin_google_merchants_path, t("google_merchants_description")) %>)
 )
 
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menua",
                      :name => "insert_google_merchant_into_admin_configuration_sidebar_menu",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
                      :text => "<%= configurations_sidebar_menu_item t('google_merchant'), admin_google_merchants_path %>",
