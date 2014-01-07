5a15f70004e7564128b00b1ed4cb29a17facfde0
Enrique Alvarez <enrique@codecantor.com>
2014-01-07 12:00:25 +0100

    Adapt to rails 4 with get not match

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.append do
-  match "google_merchant", :to => 'products#google_merchant'
+  get "google_merchant", :to => 'products#google_merchant'
 
   namespace :admin do
     resource :google_merchants
